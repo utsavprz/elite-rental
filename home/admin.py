a636b213675211868df84585b97d6d9bf1ea4b2a
utsavprz <Madridista0o>
2021-09-29 15:15:36 +0545

    Added newletter in home page

--- home/admin.py
+++ home/admin.py
@@ -1,1 +1,10 @@
 from django.contrib import admin
+from .models import newsletter
+
+class listsub(admin.ModelAdmin):
+    list_display= ('user_id','email')
+    
+
+    def has_add_permission(self, request):
+        return False
+admin.site.register(newsletter,listsub)
--- home/views.py
+++ home/views.py
@@ -1,11 +1,33 @@
 from django.shortcuts import render
+from .models import newsletter
 
 
 # Create your views here.
 
 def index(request):
-    context={
+    current_user = request.user
 
+    if request.method == "POST":
+        user_id_id = current_user.id
+        email = request.POST.get('email')
+
+        sub = newsletter(user_id_id = user_id_id, email=email)
+        sub.save()
+        print("Newsletter subbed")
+    
+    exists = ""
+
+    if_exists= newsletter.objects.filter(user_id = current_user).count()
+
+    if if_exists == 1:
+        exists = "Yes"
+    else:
+        exists ="No"
+    
+
+    context={
+        'current_user':current_user,
+        'exists': exists,
     }
     return render(request,'home/index.html',context)
 
