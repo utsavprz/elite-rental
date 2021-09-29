74df22767c308c6fe91b9dc45dbc95872ba6d11f
utsavprz <Madridista0o>
2021-09-29 22:58:54 +0545

    first verion

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn eliterental.wsgi --log-file -
--- eliterental/settings.py
+++ eliterental/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7ba1!0o-smscnch_ju1zo!2wmj53%f*2&%h8-dyb2wue^igvue'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*", "eliterental.herokuapp.com"]
 
 
 # Application definition
